f8a1911f0ffdd89e948378c77a10bc1b2e6721a0
MinSeo Cho <chj2171@gmail.com>
2022-12-17 22:48:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.57 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,10 +1,11 @@
 class Solution {
-    static int arr[] = new int[100002];
+    static int arr[] = new int[100001];
     public int solution(int n) {
-        arr[0] =0;
-        arr[1] =1;
+        int answer = 0;
+        arr[0]=0;
+        arr[1]=1;
         for(int i=2; i<=n; i++){
-            arr[i] = (arr[i-1]+arr[i-2])%1234567;
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
         }
         return arr[n];
     }
