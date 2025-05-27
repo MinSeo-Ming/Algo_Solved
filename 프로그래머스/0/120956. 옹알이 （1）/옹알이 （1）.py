8dc46f290c9764d38a52149207d786c3e737601c
MinSeo Cho <chj2171@gmail.com>
2025-05-27 16:08:27 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -5,7 +5,4 @@ def solution(babbling):
     
     all = {''.join(perm) for r in range(1,len(possible)+1) for perm in permutations(possible,r)}
     
-    for ba in babbling:
-        if ba in all:
-            answer +=1
-    return answer
+    return sum(1 for ba in babbling if ba in all )
