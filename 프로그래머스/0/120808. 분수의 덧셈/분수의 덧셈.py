30144398a750447ca3236d8993897b9b6ed55b55
MinSeo Cho <chj2171@gmail.com>
2025-07-16 14:04:02 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,21 @@
-import math
+def gg(num1,num2):
+    temp=0
+    if num2> num1:
+        temp = num1
+        num1 =num2
+        num2= temp
+    while num2 >0:
+        num1,num2 = num2, num1 %num2
+    return num1
 
 def solution(numer1, denom1, numer2, denom2):
-    up = numer1 * denom2 + numer2 * denom1
-    down = denom1 * denom2
-    gcb = math.gcd(up, down)  
+    num = numer1 * denom2 + numer2 * denom1
+    demon =denom1 * denom2
+    gc = gg(num,demon)
+    num = num/gc
+    demon = demon/gc
+    print(numer1,denom1,numer2,denom2,num,demon)
     
-    answer = [0,0] 
-    answer[0] = up/gcb
-    answer[1] = down/gcb
+    answer = [int(num),int(demon)]
     return answer
+
