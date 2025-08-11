fdcfb9412e64834a1edd982a949d59f84a964efe
MinSeo Cho <chj2171@gmail.com>
2025-08-11 15:03:12 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,7 @@
+def solution(my_string, s, e):
+    print( my_string[:s])
+    print(''.join(reversed(my_string[s:e+1])))
+    print(my_string[e+1:])
+    answer =  my_string[:s] +''.join(reversed(my_string[s:e+1]))+my_string[e+1:]
+    
+    return answer
