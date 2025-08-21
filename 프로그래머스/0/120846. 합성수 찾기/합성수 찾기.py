0251e019c0588da272968e0f6c0a99a5ff68b9af
MinSeo Cho <chj2171@gmail.com>
2025-08-21 17:19:34 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,10 +1,5 @@
+temp =[0]*100
 def solution(n):
-    primes = [True] * (n+1)
-    primes[0] = primes[1] = False
-    
-    for i in range(2, int(n**0.5)+1):
-        if primes[i]:
-            for j in range(i*i, n+1, i):
-                primes[j] = False
-    
-    return n- len([i for i, prime in enumerate(primes) if prime])-1
+    answer = 0
+    print(temp)
+    return answer
