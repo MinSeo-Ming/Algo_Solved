8c4b5b56df8f93db3dfe68cb6e7a55685626cdb9
MinSeo Cho <chj2171@gmail.com>
2025-09-23 17:48:42 +0900

    [level 1] Title: 모의고사, Time: 3.76 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,29 +1,18 @@
 def solution(answers):
-    # 사람으로 돌려가며 비교
-        # 문제랑 정답이랑 비교한다 
-        # 문제 반복해서 찍으니 
-        # 길이 만큼만 반복해서 한다.
-        #맞을 경우 +1
-    # 최고값과 비교하여 같으면 인덱스 추가
-    # 높으면 전체다 지우고 현재만 추가
-    # 낮으면 스킵
-    answer = []
-    people =[[1, 2, 3, 4, 5],[2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    MAX=-1
-    order =0
-    for person in (people):
-        le=len(person)
-        sum=0
-        order +=1
-        for i in range(len(answers)):
-            idx = i%le
-            if person[idx] == answers[i]:
-                sum+=1
-        
-        if(MAX<sum):
-            MAX=sum
-            answer.clear()
+    people=[[0 ],[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
+    answer=[]
+    max_answer=0
+    for order in range(1,4):
+        p =people[order]
+        count=0
+
+        for idx,a in enumerate(answers):
+            if a ==p[idx%len(p)]:
+                count+=1
+        if max_answer==count:
             answer.append(order)
-        elif(MAX==sum):
+        elif max_answer<count:
+            answer =[]
             answer.append(order)
+            max_answer =count
     return answer
